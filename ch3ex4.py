f579e8ffb3f3cf6256a5040cef1026559115f1cc
Hoyt Chang <hoyt.chang@gmail.com>
2019-03-09 16:14:45 -0500

    elliptic curves over finite fields

--- ch3ex4.py
+++ ch3ex4.py
@@ -1,9 +1,15 @@
 from ecc import FieldElement, Point
 # Exercise 4
-# For the curve y**2 = x**3 + 7 over F_223, find 21*(47,71)
+# For the curve y**2 = x**3 + 7 over F_223, find 2*(192,105)
 prime = 223
 a = FieldElement(0,prime)
 b = FieldElement(7,prime)
+x = FieldElement(192,prime)
+y = FieldElement(105,prime)
+p = Point(x,y,a,b)
+print('2 * (192,105) = ' + str(2*p))
+
+# For the curve y**2 = x**3 + 7 over F_223, find 21*(47,71)
 x = FieldElement(47,prime)
 y = FieldElement(71,prime)
 p = Point(x,y,a,b)
--- ecc.py
+++ ecc.py
@@ -12,13 +12,11 @@ class FieldElement:
 	def __eq__(self, other):
 		if other is None:
 			return False
-		if isinstance(other,FieldElement):
-			return self.num == other.num and self.prime == other.prime
-		else:
-			return False
+		return self.num == other.num and self.prime == other.prime
 	
 	def __ne__(self, other):
-		return self.num != other.num or self.prime != other.prime
+		return not (self == other)
+		#return self.num != other.num or self.prime != other.prime
 
 	def __repr__(self):
 		return 'FieldElement_{}({})'.format(self.prime, self.num)
@@ -61,8 +59,6 @@ class FieldElement:
 
 class Point:
 
-	zero = 0
-
 	def __init__(self, x, y, a, b):
 		self.a = a
 		self.b = b
@@ -86,7 +82,8 @@ class Point:
 		return self.a == other.a and self.b == other.b and self.x == other.x and self.y == other.y
 
 	def __ne__(self, other):
-		return self.a != other.a or self.b != other.b or self.x != other.x or self.y != other.y
+		return not (self == other)
+		#return self.a != other.a or self.b != other.b or self.x != other.x or self.y != other.y
 
 	def __repr__(self):
 		if self.x is None:
@@ -99,38 +96,57 @@ class Point:
 		#return 'Point ({},{}) on curve a,b={},{}'.format(self.x, self.y, self.a, self.b)
 
 	def __add__(self, other):
+		if self.a != other.a or self.b != other.b:
+			raise TypeError("Points {}, {} are not on the same curve".format(self,other))
+
+		# Case 0.0: self is the point at infinity, return other
 		if self.x is None:
 			return other
+		
+		# Case 0.1: other is the point at infinity, return self
 		if other.x is None:
 			return self
+
+		# Case 1: self.x == other.x, self.y != other.y
+		# Result is point at infinity
+		if self.x == other.x and self.y != other.y:
+			return self.__class__(None, None, self.a, self.b)
+
+		# Case 2: self.x != other.x
 		if self.x != other.x:
 			s = (other.y - self.y)/(other.x - self.x)
 			x = s**2 - self.x - other.x
 			y = s*(self.x - x) - self.y
 			return self.__class__(x, y, self.a, self.b)
-		if self == other and self.y == self.zero:
+		
+		# Case 4: if we are tangent to the vertical line
+		# Return point at infinity
+		# instead of figuring out what 0 is for each type
+		# we just use 0 * self.x
+		if self == other and self.y == 0 * self.x:
 			return self.__class__(None, None, self.a, self.b)
+
+		# Case 3: self == other
 		if self.x == other.x and self.y == other.y:
 			s = (3*self.x**2 + self.a)/(2*self.y)
 			x = s**2 - 2*self.x
 			y = s*(self.x - x) - self.y
 			return self.__class__(x, y, self.a, self.b)
-		if self.x == other.x:
-			return self.__class__(None, None, self.a, self.b)
-
-	def __rmul__(self, coefficient):
-		product = self.__class__(None,None,self.a,self.b)
-		for _ in range(coefficient):
-			product += self
-		return product
+		
 
 	#def __rmul__(self, coefficient):
-	#	self.bits = math.ceil(math.log(coefficient,2))
-	#	current = self
-	#	result = Point(None,None,self.a,self.b)
-	#	for _ in range(self.bits):
-	#		if coefficient & 1:
-	#			result += current
-	#		current += current
-	#		coefficient >>= 1
-	#	return result
+	#	product = self.__class__(None,None,self.a,self.b)
+	#	for _ in range(coefficient):
+	#		product += self
+	#	return product
+
+	def __rmul__(self, coefficient):
+		coef = coefficient
+		current = self
+		result = self.__class__(None,None,self.a,self.b)
+		while coef:
+			if coef & 1:
+				result += current
+			current += current
+			coef >>= 1
+		return result
